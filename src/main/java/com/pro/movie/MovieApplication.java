5b15e68ed7740a92137b8f4e06d8c2a7c3d0ecc3
Deba Prasad Sinha <dsinha@advatix.com>
2022-01-29 18:21:09 +0530

    changes in application file

--- src/main/java/com/pro/movie/MovieApplication.java
+++ src/main/java/com/pro/movie/MovieApplication.java
@@ -2,15 +2,22 @@ package com.pro.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class MovieApplication {
+public class MovieApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	    return builder.sources(MovieApplication.class);
 	}
 
 }
