857bb23b6d43775544a0294555e470b279db9075
Deba Prasad Sinha <dsinha@advatix.com>
2022-01-29 14:16:20 +0530

    add movie

---
+++ src/main/java/com/pro/movie/model/Movie.java
@@ -0,0 +1,24 @@
+package com.pro.movie.model;
+
+public class Movie {
+	private String name;
+	private String actor;
+	public Movie() {
+	}
+	public Movie(String name, String actor) {
+		this.name = name;
+		this.actor = actor;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getActor() {
+		return actor;
+	}
+	public void setActor(String actor) {
+		this.actor = actor;
+	}
+}
